7dd3212aa31ac5587bf9c125ef6e1365b817057c
yoann <yoann.ochietti@gmail.com>
2018-03-02 21:08:20 -0500

    Dancing() now has fast intro, random choice of middle section using case statement with 5 programmed dance modes, and a finale with a crescendo, a fast sustain and a two legged kick.

--- boiteadons.ino
+++ boiteadons.ino
@@ -1,7 +1,7 @@
 //Code copied from user PaulStoffregen on the teensy forums at the following adress:
 //https://forum.pjrc.com/threads/48308-test-code-for-piezo
 
-const int thresholdMin = 100;  // minimum reading, avoid "noise"
+const int thresholdMin = 120;  // minimum reading, avoid "noise"
 const int aftershockMillis = 150; // time of aftershocks & vibration
 const int velocityOffset = 10;
 
@@ -9,9 +9,12 @@ const int velocityOffset = 10;
 const bool withDance = true;
 const int numMotors = 2;
 const int motorPins[numMotors] = {11, 12};
-const int numSteps = 24;
-const int fastSustainSteps = 12;
+const int crescendoSteps = 12;
+const int midSteps = 16;
+//intro and end
+const int fastSustainSteps = 6;
 
+//debug mode
 const int rawOnly = false;
 
 int state = 0; // 0=idle, 1=looking for peak, 2=ignore aftershocks
@@ -36,7 +39,7 @@ void setup() {
 
 void loop() {
   int piezo = analogRead(A0);
-  if (rawOnly){
+  if (rawOnly) {
     Serial.println(piezo);
   }
   else if (state == 0) {
@@ -80,8 +83,85 @@ void dance() {
   int longPulse = 250;
   int shortPulse = 175;
 
-  int increments = (longDelay - shortDelay) / numSteps;
 
+  //Fast
+  for (int i = 0; i < fastSustainSteps; i++) {
+    //right leg seems to prefer long pulses???
+    pulseMotor(0, shortPulse);
+    delay(shortDelay);
+    pulseMotor(1, shortPulse);
+    delay(shortDelay);
+  }
+  randomSeed(analogRead(A1));
+  int type = random(0, 5);
+  switch (type) {
+    case 0:
+      // Roomba mid
+      Serial.println("ROOMBA");
+      for (int i = 0; i < midSteps; i++) {
+        if (i % 2 == 0) {
+          pulseMotor(0, 200);
+          delay(600);
+          pulseMotor(1, 200);
+          delay(200);
+        } else {
+          pulseMotor(1, 200);
+          delay(200);
+          pulseMotor(0, 200);
+          delay(600);
+        }
+      }
+      break;
+
+    case 1:
+      // 4/4
+      Serial.println("4/4");
+      for (int i = 0; i < midSteps; i++) {
+        pulseMotor(0, 200);
+        pulseMotor(1, 200);
+        delay(400);
+      }
+      break;
+
+    case 2:
+      //Crescendo mid
+      Serial.println("Crescendo");
+      for (int delayDuration = longDelay; delayDuration >= shortDelay; delayDuration -= (longDelay - shortDelay) / midSteps) {
+        int pulseDuration = map(delayDuration, shortDelay, longDelay, shortPulse, longPulse);
+        pulseMotor(0, pulseDuration);
+        delay(delayDuration);
+        pulseMotor(1, pulseDuration);
+        delay(delayDuration);
+      }
+      break;
+
+    case 3:
+      // 6/8
+      Serial.println("6/8");
+      for (int i = 0; i < midSteps; i++) {
+        pulseMotor(0, 200);
+        delay(200);
+        pulseMotor(1, 200);
+        delay(200);
+      }
+      break;
+
+    case 4:
+      //Crescendo long
+      Serial.println("long crescendo");
+      int incrementsLong = (longDelay - shortDelay) / midSteps * 2;
+      for (int delayDuration = longDelay; delayDuration >= shortDelay; delayDuration -= incrementsLong) {
+        int pulseDuration = map(delayDuration, shortDelay, longDelay, shortPulse, longPulse);
+        pulseMotor(0, pulseDuration);
+        delay(delayDuration);
+        pulseMotor(1, pulseDuration);
+        delay(delayDuration);
+      }
+      break;
+  }
+
+  //Crescendo finish
+  int increments = (longDelay - shortDelay) / crescendoSteps;
   for (int delayDuration = longDelay; delayDuration >= shortDelay; delayDuration -= increments) {
     int pulseDuration = map(delayDuration, shortDelay, longDelay, shortPulse, longPulse);
     pulseMotor(0, pulseDuration);
@@ -89,24 +169,23 @@ void dance() {
     pulseMotor(1, pulseDuration);
     delay(delayDuration);
   }
-  for (int i = 0; i < fastSustainSteps; i++) {
 
+  //Fast finish
+  for (int i = 0; i < fastSustainSteps; i++) {
     //right leg seems to prefer long pulses???
-    pulseMotor(0, longPulse);
+    pulseMotor(0, shortPulse);
     delay(shortDelay);
     pulseMotor(1, shortPulse);
     delay(shortDelay);
   }
 
+  //Finale
   delay(1500);
-
   digitalWrite(motorPins[0], HIGH);
   digitalWrite(motorPins[1], HIGH);
   delay(300);
   digitalWrite(motorPins[0], LOW);
   digitalWrite(motorPins[1], LOW);
-
-
 }
 
 void pulseMotor (int motor, int pulseDuration) {
